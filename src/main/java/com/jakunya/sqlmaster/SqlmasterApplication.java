959bb7827095bca1d5f30ebef8a809cefdc55e12
Holkir <ataevdavid4@gmail.com>
2026-01-10 17:17:58 +0300

    Initial commit of the Spring Boot project structure

---
+++ src/main/java/com/jakunya/sqlmaster/SqlmasterApplication.java
@@ -0,0 +1,13 @@
+package com.jakunya.sqlmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlmasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlmasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jakunya/sqlmaster/SqlmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakunya.sqlmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlmasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
